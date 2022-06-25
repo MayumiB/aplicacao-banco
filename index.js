1d033a811cbf21f5ee6567831541c21bacba3e66
MayumiB <dmayumibarbosa@gmail.com>
2022-06-25 10:52:36 -0300

    criando classes

---
+++ index.js
@@ -0,0 +1,25 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+     cliente1.nome = "Jose";
+     cliente1.cpf = 11122233304;
+     cliente1.agencia = 1001;
+     cliente1.saldo = 0;
+
+     cliente2.nome = "Maria Alice";
+     cliente2.cpf = 22211133304;
+     cliente2.agencia = 1001;
+     cliente2.saldo = 0;
+
+console.log(cliente1, cliente2);
+
+
+
