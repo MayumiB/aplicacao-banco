87923440ae112f63e7d551137e80f560eb2f7a95
MayumiB <dmayumibarbosa@gmail.com>
2022-06-27 17:49:41 -0300

    adicionando modulos

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-class Cliente{
-    nome;
-    cpf;
-    agencia;
-    saldo;
-
-}
+import {Cliente} from "./Cliente.js"
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
 
      cliente1.nome = "Jose";
      cliente1.cpf = 11122233304;
-     cliente1.agencia = 1001;
-     cliente1.saldo = 0;
 
      cliente2.nome = "Maria Alice";
      cliente2.cpf = 22211133304;
-     cliente2.agencia = 1001;
-     cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteJose = new ContaCorrente();
+contaCorrenteJose.agencia = 1001;
+
+contaCorrenteJose.depositar(-100);
+contaCorrenteJose.depositar(100);
+contaCorrenteJose.depositar(100);
+
+const valorSacado = contaCorrenteJose.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteJose);
 
 
 
